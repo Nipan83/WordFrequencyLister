143be8b886dd12398f1510c8575cf2a745880c9c
NIPAN DAS <nipandas83@gmail.com>
2018-02-22 13:07:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-const port = process.env.PORT || 3005
+const port = process.env.PORT || 3000
 
 app.post('/send', function (req, res) {
   var response = res
